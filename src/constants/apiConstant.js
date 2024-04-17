85dc3c5831a81fc5ad7bea855d394334e8d26862
Lopez Nicolas <richtofen60@gmail.com>
2024-04-17 11:30:31 +0200

    add port to apiRoot

--- src/constants/apiConstant.js
+++ src/constants/apiConstant.js
@@ -1,5 +1,5 @@
 // on déclare l'url de base
-export const apiRoot = "http://api_kigo.lndo.site";
+export const apiRoot = "http://api_kigo.lndo.site:8000";
 
 // on déclare l'url de notre API
 export const apiUrl =`${apiRoot}/api`;
