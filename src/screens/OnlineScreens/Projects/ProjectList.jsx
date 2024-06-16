d005e78a3d86c95c78e119d02575c4bfb711ed59
SkyBlyvion <richtofen60@gmail.com>
2024-06-16 21:22:53 +0200

    add method to fetch posts

--- src/screens/OnlineScreens/Projects/ProjectList.jsx
+++ src/screens/OnlineScreens/Projects/ProjectList.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { apiUrl, apiRoot } from '../../../constants/apiConstant';
 import PageLoader from '../../../components/loader/PageLoader';
-import { Link } from 'react-router-dom';
 
 const ProjectsList = () => {
   const [projects, setProjects] = useState([]);
@@ -39,11 +38,16 @@ const ProjectsList = () => {
             })
           );
 
+          // Fetch post details
+          const postResponse = await axios.get(`${apiRoot}${project.post}`);
+          const postDetails = postResponse.data;
+
           return {
             ...project,
             competences: competencesDetails,
             filieres: filieresDetails,
             users: usersDetails,
+            postDetails,
           };
         }));
 
@@ -60,60 +64,52 @@ const ProjectsList = () => {
 
   if (loading) return <PageLoader />;
 
-  console.log('projects', projects);
+  console.log('Projects:', projects);
   return (
-    <div className='flex flex-col items-center h-screen overflow-y-auto p-4 pb-20'>
-        <div className="p-4 w-full flex justify-between items-center">
-          <Link to="/CreatePost"> {/*TODO:remplacer ici par createpost*/}
-            <img src="../../../../documentation/svg/edit.svg" alt="setting" className="text-2xl" />
-          </Link>
-        </div>
-        {/* <img src="../../../../documentation/svg/Filter.svg" alt="filter" /> */}
-      <h1 className="text-2xl text-orange font-bold pb-4">Projects</h1>
-      {/* <p>Un porteur de projet peut créer un projet dans l’application pour y 
+    <div className='flex flex-col items-center text-center px-5'>
+      <img src="../../../../documentation/svg/Filter.svg" alt="filter" />
+      <div>Projects List</div><br/>
+      <p>Un porteur de projet peut créer un projet dans l’application pour y 
         spécifier les besoins en compétences pour le réaliser, 
         ainsi que les filières professionnelles nécessaires. 
         Le projet est associé à la création à un post et une ou plusieurs photos 
         accompagné d’un texte descriptif.</p><br/>
       <p>Un utilisateur peut explorer les projets proposés par le porteur et 
-        demander à y participer selon les compétences demandées.</p><br/> */}
-      <div className='w-5/6'>
+        demander à y participer selon les compétences demandées.</p><br/>
+      <div className='w-full'>
         {projects.map(project => (
           <div key={project.id} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-            <h2 className='text-xl font-bold'>{project?.post?.title}Titre:</h2>
-            <p>{project?.post?.text}Text:</p>
-            <p>{project?.post?.created_date}C_at:</p>
-            <p>{project?.post?.updated_date}Upd_at:</p>
-            <p>{project?.post?.type}typedeprojet:</p>
-            {project?.post?.media && (
+            <h2 className='text-xl font-bold'>{project.postDetails.title}</h2>
+            <p>{project.postDetails.text}</p>
+            <p>Type de projet: {project.postDetails.type}</p>
+            <p>Créé le: {new Date(project.postDetails.created_date).toLocaleDateString()}</p>
+            <p>Mis à jour le: {new Date(project.postDetails.updated_date).toLocaleDateString()}</p>
+            {project.postDetails.media && (
               <div>
-                <p>{project?.post?.media?.label}</p>
-                <img src={`http://api_kigo.lndo.site/images/postImages/${project?.post?.media?.url_img}`} alt={project?.post?.media?.label} className="mt-4 img-post" />
+                <p>{project.postDetails.media.label}</p>
+                <img src={`http://api_kigo.lndo.site/images/postImages/${project.postDetails.media.url_img}`} alt={project.postDetails.media.label} className="mt-4 img-post" />
               </div>
             )}
-            <br />
             <div>
-              <h3 className='text-xl font-bold'>Compétences:</h3>
+              <h3>Compétences</h3>
               <ul>
-                {project?.competences.map(comp => (
-                  <li key={comp.id}> - {comp.label}</li>
+                {project.competences.map(comp => (
+                  <li key={comp.id}>{comp.label}</li>
                 ))}
               </ul>
-              <br />
             </div>
             <div>
-              <h3 className='text-xl font-bold'>Filières:</h3>
+              <h3>Filières</h3>
               <ul>
-                {project?.filieres.map(fil => (
-                  <li key={fil.id}> - {fil.label}</li>
+                {project.filieres.map(fil => (
+                  <li key={fil.id}>{fil.label}</li>
                 ))}
               </ul>
-              <br />
             </div>
             <div>
-              <h3 className='text-xl font-bold'>Porteur:</h3>
+              <h3>Utilisateurs</h3>
               <ul>
-                {project?.users.map(user => (
+                {project.users.map(user => (
                   <li key={user.id}>{user.firstname} {user.lastname}</li>
                 ))}
               </ul>
